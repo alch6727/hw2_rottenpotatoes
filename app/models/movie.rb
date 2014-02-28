e1844db95875cbf042b7a6293e21ca71a9dc2dd8
alch6727 <alice.chien@colorado.edu>
2014-02-28 00:02:45 -0400

    index changes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,10 @@
 class Movie < ActiveRecord::Base
 	def self.all_ratings
+		#Obtains all the movies using rating and pushes them on an array
 		array = Array.new
 		self.select("rating").uniq.each {|e1| array.push(e1.rating)}
+
+		#Returns the sorted array
 		array.sort.uniq
 	end
 end
